0495ea05481a136a90571e6b2d9c10b6a7aefd0b
eduardo Alves <eduardo.telecomrbs@gmail.com>
2023-06-27 10:23:57 -0300

    minha_loja_alterações_finais

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-
 class Produto {
 
     constructor() {
         this.id = 1;
         this.arrayProdutos = [];
+        this.editId = null;
     }
-
 
     save() {
         let produto = this.lerDados();
+
         if (this.validaCampos(produto)) {
-            this.adicionar(produto)
+            if (this.editId == null) {
+                this.adicionar(produto);
+            } else {
+                this.toUpdate(this.editId, produto);
+            }
         }
-        this.listaTabela()
+
+        this.listaTabela();
         this.cancel();
     }
 
-
     listaTabela() {
-        let tbody = document.getElementById('tbody');
+        let tbody = document.getElementById('tbody')
         tbody.innerText = '';
 
         for (let i = 0; i < this.arrayProdutos.length; i++) {
@@ -28,7 +32,7 @@ class Produto {
             let td_produto = tr.insertCell();
             let td_valor = tr.insertCell();
             let td_acoes = tr.insertCell();
-
+            //edição de botão
             td_id.innerText = this.arrayProdutos[i].id;
             td_produto.innerText = this.arrayProdutos[i].nomeProduto;
             td_valor.innerText = this.arrayProdutos[i].valor;
@@ -36,13 +40,15 @@ class Produto {
             //alinhando ao centro 
             td_id.classList.add('center');
 
-            //adicionando imagens dos botões
+            //botões e ações (excluir/editar)
             let imgEdit = document.createElement('img');
             imgEdit.src = 'img/pencil-square.svg';
+            imgEdit.setAttribute("onclick", "produto.preparaEdit(" + JSON.stringify(this.arrayProdutos[i]) + ")");
+
 
             let imgDelet = document.createElement('img');
             imgDelet.src = 'img/trash.svg';
-            imgDelet.setAttribute("onclick", "produto.deletar("+this.arrayProdutos[i].id+")");
+            imgDelet.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id + ")");
 
             td_acoes.appendChild(imgEdit);
             td_acoes.appendChild(imgDelet);
@@ -50,8 +56,27 @@ class Produto {
         }
     }
     adicionar(produto) {
+        produto.valor =parseFloat(produto.valor)
         this.arrayProdutos.push(produto);
         this.id++;
+    }
+
+    toUpdate(id, produto) {
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if (this.arrayProdutos[i].id == id) {
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+                this.arrayProdutos[i].valor = produto.valor;
+            }
+        }
+    }
+    preparaEdit(dados) {
+        this.editId = dados.id;
+
+        document.getElementById('produto').value = dados.nomeProduto;
+        document.getElementById('valor').value = dados.valor;
+
+        document.getElementById('btn1').innerText = 'Atualizar';
+
     }
 
     lerDados() {
@@ -81,18 +106,23 @@ class Produto {
     cancel() {
         document.getElementById('produto').value = '';
         document.getElementById('valor').value = '';
+
+        document.getElementById('btn1').innerText = 'Salvar';
+        this.editId = null;
+
     }
+    //Método Deletar 
+    deletar(id) {
+        if (confirm('Deseja Realmente deletar o produto?')) {
+            let tbody = document.getElementById('tbody');
 
-    deletar(id){
-        let tbody = document.getElementById('tbody');
-
-            for(let i = 0; i < this.arrayProdutos.length; i++){
-                if(this.arrayProdutos[i].id == id){
+            for (let i = 0; i < this.arrayProdutos.length; i++) {
+                if (this.arrayProdutos[i].id == id) {
                     this.arrayProdutos.splice(i, 1);
                     tbody.deleteRow(i);
                 }
             }
+        }
     }
-
 }
-    let produto = new Produto();
+let produto = new Produto();
